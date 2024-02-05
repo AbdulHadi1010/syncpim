2b068906f9bc84d385a1f36bb5dcfd539ed90bf8
Abdul Hadi <hadi2408466@gmail.com>
2024-02-05 11:18:14 +0500

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './contants';
 import { UsersService } from 'src/users/users.service';
@@ -14,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
